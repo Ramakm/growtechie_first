e34c92e39f7ec8fa98d3a26b49beef47cf1cca0b
Palak Goyal <Palakgoyal0304@gmail.com>
2024-02-11 12:54:58 +0000

    change hero UI

---
+++ src/assets/hero/index.js
@@ -0,0 +1,9 @@
+import heroImgBoy from "./hero-img-boy.webp";
+import heroImgWoman from "./hero-img-woman.png";
+import heroImgLearners from "./hero-img-learners.png"
+
+export {
+    heroImgBoy,
+    heroImgWoman,
+    heroImgLearners,
+}
