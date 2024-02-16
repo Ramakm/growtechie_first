75727b71d4d2d9e07ae258423b1bd11cb52bccd8
Palak Goyal <Palakgoyal0304@gmail.com>
2024-02-16 14:10:08 +0000

    fix faq section

---
+++ src/components/FAQ/FAQSection.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { faqData } from "../../staticData/faqData";
+import FAQItem from './FAQItem';
+
+const FAQSection = () => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const toggleAccordion = (index) => {
+        setActiveIndex(activeIndex === index ? null : index);
+    };
+
+    let faqDivide = Math.ceil(faqData.length/2);
+    let firstHalfFaq = faqData.slice(0, faqDivide);
+    let secondHalfFaq = faqData.slice(faqDivide, faqData.length);
+
+    return (
+        <div className="flex justify-between gap-8 mt-10">
+            <div className="w-1/2">
+                {firstHalfFaq.map((faq, index) => (
+                    <FAQItem
+                        faq={faq}
+                        index={index}
+                        toggleAccordion={toggleAccordion}
+                        activeIndex={activeIndex}
+                    />
+                ))}
+            </div>
+            <div className="w-1/2">
+                {secondHalfFaq.map((faq, index) => (
+                    <FAQItem
+                        faq={faq}
+                        index={index + faqDivide}
+                        toggleAccordion={toggleAccordion}
+                        activeIndex={activeIndex}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default FAQSection;
---
+++ src/staticData/faqData.js
@@ -0,0 +1,47 @@
+export const faqData = [
+    {
+      question: 'What Will You Learn?',
+      answer:
+        'We provide a diverse array of courses and programs, with a primary focus on areas such as Python, SQL, Software Testing, Data Science, and Data Analytics. Our core philosophy centers around imparting practical, real-world skills, prioritizing hands-on learning over theoretical instruction.',
+    },
+    {
+      question: 'Is It Free or Paid?',
+      answer:
+        'The course is not free; instead, it begins with a demo session. If you find the demo valuable, you can choose to enroll in the full course for a nominal fee, which is significantly lower than what other e-learning platforms charge.',
+    },
+    {
+      question: 'What will you gain?',
+      answer:
+        'Embark on a journey to gain valuable hands-on experience and receive expert guidance that will empower you to kickstart your career. At our institution, we\'re dedicated to equipping you with all the essential knowledge and skills you need to excel in your chosen field.',
+    },
+    {
+      question: 'Do We Offer Certifications?',
+      answer:
+        'While we do offer a participation certification to acknowledge your commitment to the course, it\'s important to remember that our motto is \'Knowledge over Certification.\' We strongly encourage you to prioritize knowledge acquisition over simply pursuing a certificate.',
+    },
+    {
+      question: 'Why GrowTechie?',
+      answer:
+        'We are a close-knit community of tech enthusiasts committed to sharing our knowledge. Our primary focus is on students, not profits. We don\'t impose fixed prices for our courses, and we don\'t expect participants to work on projects independently. Instead, our approach is entirely live, offering real-time assistance and full transparency. Join us on this journey of growth!',
+    },
+    {
+      question: 'Are we available offline?',
+      answer:
+        'At present, our offerings are exclusively available online. We believe in the effectiveness of online education, especially in light of the lessons from the COVID-19 era. It allows for flexible schedules, reduces the need for travel, and empowers individuals to work and study at their own comfortable pace.',
+    },
+    {
+      question: 'What payment modes are accepted?',
+      answer:
+        'We accept all kinds of online payment Netbanking, UPIs or Paypal.',
+    },
+    {
+      question: 'What if I miss a live class in Growtechie?',
+      answer:
+        'We kindly request that you send us an email in advance if you anticipate missing a class, as all our classes will be conducted live online. We will ensure that you receive the recording of the missed session directly to your email.',
+    },
+    {
+      question: 'What if I have more queries?',
+      answer:
+        'Please contact our course advisor, Mob: +91-7978939845, Email: growtechie.ind@gmail.com.',
+    },
+  ];
