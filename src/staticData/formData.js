91f85b87c7c494e676be731d246c82f81b528150
palakkgoyal <palakgoyal0304@gmail.com>
2024-03-05 18:48:56 +0530

    refactor form page

--- src/staticData/formData.js
+++ src/staticData/formData.js
@@ -1,50 +1,54 @@
 export const coursesArr = ["python-programming", "python-projects", "SQL", "power-BI", "data-science", "data-analytics", "software-testing", "CS-GATE-college-subjects", "tech-career-growth"]
 
-const radioData = [
+
+export const data = [
+    {
+        label: "Name of the Learner",
+        name: "name",
+        inputType: "text",
+    },
+    {
+        label: "Email",
+        name: "email",
+        inputType: "text",
+    },
+    {
+        label: "Mobile No",
+        name: "phone",
+        inputType: "text",
+    },
     {
         name: "higherEducation",
         label: "Higher Degree Education",
-        values: ["B.Tech", "BE", "Diploma Degree", "Masters Degree", "Other"]
+        values: ["B.Tech", "BE", "Diploma Degree", "Masters Degree", "Other"],
+        inputType: "radio",
     },
     {
         name: "courseLearningOption",
         label: "Course Learning Option",
-        values: ["Group Session", "1-1 Session"]
+        values: ["Group Session", "1-1 Session"],
+        inputType: "radio",
+    },
+    {
+        label: "Course Fee? Lerner interested to give",
+        name: "fee",
+        inputType: "text",
     },
     {
         name: "paymentOptions",
         label: "Payments Options",
-        values: ["UPI", "Net Banking"]
+        values: ["UPI", "Net Banking"],
+        inputType: "radio",
+    },
+    {
+        label: "Course time learner interested to give",
+        name: "time",
+        inputType: "text",
     },
     {
         name: "findUs",
         label: "Where did you find about us?",
-        values: ["LinkedIN", "Facebook", "Instagram", "Twitter", "Others"]
+        values: ["LinkedIN", "Facebook", "Instagram", "Twitter", "Others"],
+        inputType: "radio",
     },
 ]
-
-export const textData = [
-    {
-        label: "Name of the Learner",
-        name: "name"
-    },
-    {
-        label: "Email",
-        name: "email"
-    },
-    {
-        label: "Mobile No",
-        name: "phone"
-    },
-    {
-        label: "Course Fee? Lerner interested to give",
-        name: "fee"
-    },
-    {
-        label: "Course time learner interested to give",
-        name: "time"
-    },
-]
-
-
-export default radioData
