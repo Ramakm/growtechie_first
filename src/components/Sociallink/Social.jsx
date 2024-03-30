a19c1efa8689344c5c1e60b375aad3b52a50b8a1
Palak Goyal <Palakgoyal0304@gmail.com>
2024-03-30 12:43:40 +0000

    fix social link size on small device

--- src/components/Sociallink/Social.jsx
+++ src/components/Sociallink/Social.jsx
@@ -1,22 +1,33 @@
 const Social = () => {
   return (
     <div className="fixed right-0 top-1/4 z-50 px-5 py-3 bg-transparent flex flex-col space-y-3">
-      
       <a
         className="footer-icons"
         href="https://www.linkedin.com/company/growtechieind"
         title="Share on LinkedIn"
         target="_blank"
       >
-        <img src="/assets/linkedin.svg" alt="LinkedIn" width="40" height="40" />
+        <img
+          src="/assets/linkedin.svg"
+          alt="LinkedIn"
+          width="40"
+          height="40"
+          className="w-6 h-6 md:w-10 md:h-10"
+        />
       </a>
-        <a
+      <a
         href="https://twitter.com/growtechie"
         className="footer-icons"
         title="Share on Twitter"
         target="_blank"
       >
-        <img src="/assets/tweet.svg" alt="Twitter" width="40" height="40" />
+        <img
+          src="/assets/tweet.svg"
+          alt="Twitter"
+          width="40"
+          height="40"
+          className="w-6 h-6 md:w-10 md:h-10"
+        />
       </a>
       <a
         href="https://www.facebook.com/profile.php?id=61551916223733"
@@ -24,7 +35,13 @@ const Social = () => {
         title="Share on Facebook"
         target="_blank"
       >
-        <img src="/assets/facebook.svg" alt="Facebook" width="40" height="40" />
+        <img
+          src="/assets/facebook.svg"
+          alt="Facebook"
+          width="40"
+          height="40"
+          className="w-6 h-6 md:w-10 md:h-10"
+        />
       </a>
 
       <a
@@ -33,9 +50,14 @@ const Social = () => {
         title="Share on Instagram"
         target="_blank"
       >
-        <img src="/assets/insta.svg" alt="Instagram" width="40" height="40" />
+        <img
+          src="/assets/insta.svg"
+          alt="Instagram"
+          width="40"
+          height="40"
+          className="w-6 h-6 md:w-10 md:h-10"
+        />
       </a>
-
     </div>
   );
 };
