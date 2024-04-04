5df67588ef16bb4cb123704eda989d15fa69f588
Palak Goyal <Palakgoyal0304@gmail.com>
2024-04-04 19:03:43 +0000

    make signup work

--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -12,8 +12,11 @@ const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    confirmedPass: "",
     remember: false,
   });
+
+  console.log(formData);
   const [isSignUp, setIsSignUp] = useState(false);
 
   const passRef = useRef(null);
@@ -46,11 +49,15 @@ const Login = () => {
 
   const handleSignUp = async (e) => {
     e.preventDefault();
+    if (formData.confirmedPass !== formData.password) {
+      alert("Password doesn't match");
+      return;
+    }
     try {
       const userCredential = await createUserWithEmailAndPassword(
         auth2,
-        email,
-        password
+        formData.email,
+        formData.password
       );
       if (formData.remember) {
         console.log(userCredential);
@@ -93,7 +100,10 @@ const Login = () => {
             : "Welcome back! Please, enter your information"}
         </p>
 
-        <form className="login-form" onSubmit={handleLogin}>
+        <form
+          className="login-form"
+          onSubmit={isSignUp ? handleSignUp : handleLogin}
+        >
           <label className="form-control__label">Email</label>
           <input
             type="email"
@@ -134,6 +144,36 @@ const Login = () => {
               />
             </svg>
           </div>
+          {isSignUp && (
+            <>
+              <label className="form-control__label">Confirm Password</label>
+              <div className="password-field">
+                <input
+                  type="password"
+                  className="form-control"
+                  minLength="8"
+                  required
+                  value={formData.confirmedPass}
+                  onChange={(e) =>
+                    setFormData({ ...formData, confirmedPass: e.target.value })
+                  }
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              </div>
+            </>
+          )}
           <div className="password__settings select-none">
             <label className="password__settings__remember">
               <input
@@ -169,7 +209,7 @@ const Login = () => {
             id="submit"
             ref={submitBtnRef}
           >
-            {isSignUp? "Sign Up" : "Log In"}
+            {isSignUp ? "Sign Up" : "Log In"}
           </button>
         </form>
         {isSignUp ? (
